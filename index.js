4a08ca1bca9a1041601ae374c81c0d21ed01ed3c
Felipe Vieira <felipe_brito@id.uff.br>
2020-08-18 20:10:50 -0300

    Criação dos PMC e Comandos de movimento

--- index.js
+++ index.js
@@ -15,7 +15,73 @@ function randomNums(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-
+//classe responsável por ser um PMC
+class Pmc{
+	constructor(l,x,y,s) {
+		this.largura=l;
+		this.posX=x;
+		this.posXAnt=x;
+		this.posY=y;
+		this.posYAnt=y;
+		this.speed=s;	
+	}
+	
+	checaMovimento(grid,x,y,l){
+		for(let i=Math.max(x-l,0); i<x+l; i++){
+			for(let j=Math.max(y-l,0); j<y+l; j++){
+				if(grid[i][j]===1){
+						return false;
+				}
+			}	
+		}
+		return true
+	}
+	//funções de movimento
+	moveCima(grid){
+		if(this.checaMovimento(grid,this.posX,this.posY-this.speed,this.largura)){
+			this.posYAnt = this.posY;
+			this.posY = this.posY - this.speed;
+		}
+	}
+	moveBaixo(grid){
+		if(this.checaMovimento(grid,this.posX,this.posY+this.speed,this.largura)){
+			this.posYAnt = this.posY;
+			this.posY = this.posY + this.speed;
+		}
+	}
+	moveEsquerda(grid){
+		if(this.checaMovimento(grid,this.posX - this.speed,this.posY,this.largura)){
+			this.posXAnt = this.posX;
+			this.posX = this.posX - this.speed;
+		}
+	}
+	moveDireita(grid){
+		if(this.checaMovimento(grid,this.posX + this.speed,this.posY,this.largura)){
+			this.posXAnt = this.posX;
+			this.posX = this.posX + this.speed;
+		}
+	}
+	
+	//atualiza a posição do pmc no grid
+	atualizaGridPMC(grid){
+		
+		//limpa a posição anterior do pmc
+		for(let i=Math.max(this.posXAnt-this.largura,0); i<this.posXAnt+this.largura; i++){
+			for(let j=Math.max(this.posYAnt-this.largura,0); j<this.posYAnt+this.largura; j++){
+				grid[i][j]=0;
+			}	
+		}
+		//realmente desenha o pmc
+		for(let i=Math.max(this.posX-this.largura,0); i<this.posX+this.largura; i++){
+			for(let j=Math.max(this.posY-this.largura,0); j<this.posY+this.largura; j++){
+				grid[i][j]=3;
+			}	
+		}
+		return grid;
+	}
+}
+	
+	
 //cria o grid
 function buildGrid() {
 	const grid = new Array(COLS).fill(null)
@@ -37,7 +103,6 @@ function buildGrid() {
 		xI = randomNums(0,grid.length-xTam);
 		for (let col = xI; col < xI+xTam; col++) {
 			for (let row = yI; row < yI+yTam; row++) {
-				console.log(col)
 				grid[col][row] = 1;
 			}
 		}
@@ -54,7 +119,6 @@ function buildGrid() {
 		xI = randomNums(0,grid.length-xTam);
 		for (let col = xI; col < xI+xTam; col++) {
 			for (let row = yI; row < yI+yTam; row++) {
-				console.log(col)
 				grid[col][row] = 2;
 			}
 		}
@@ -62,14 +126,16 @@ function buildGrid() {
 	return grid;
 }
 let grid = buildGrid();
-
+let pmc1 = new Pmc(3,2,2,2);
 //atualiza a tela usando a função update
 requestAnimationFrame(update);
 
 //função que atualizada a cada frame
 function update() {
-  render(grid);
-  requestAnimationFrame(update);
+	pmc1.moveBaixo(grid);
+	grid = pmc1.atualizaGridPMC(grid);
+	render(grid);
+	requestAnimationFrame(update);
 }
 
 //renderiza as informaçoes na tela
@@ -86,8 +152,11 @@ function render(grid) {
 	  else if(cell==1){
 		  ctx.fillStyle = 'black'
 	  }
-	  else{
+	  else if(cell==2){
 		  ctx.fillStyle = 'red'
+	  }
+	  else if(cell==3){
+		  ctx.fillStyle = 'blue'
 	  }
       ctx.fill();
       // ctx.stroke();
